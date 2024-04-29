ec78a484dbcc0f6beeeb1be01758009b352d5d8e
Skeletrox <android.svr@gmail.com>
2024-04-28 19:15:12 -0700

    [#1] Use map file as a required cmdline parameter

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod structs;
 mod filereader;
+
+use std::env;
+
 fn main() {
     println!("Hello, world!");
-    let path = "map.txt";
+    let args: Vec<String> = env::args().collect();
+    if (args.len() != 2) {
+        panic!("Wrong arguments: {args:?}, expected 1 for path got {}",
+            args.len() - 1);
+    }
+    let path = args[1].as_str();
     let (meta, map) = filereader::filereader::load_map_data_from_file(path);
     println!("Rover Map: {map:?}");
     println!("-------------------------");
